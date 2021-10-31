6f1a31e2868b10a0b60868c16fd080b2cb75233c
Alex91482 <74872120+Alex91482@users.noreply.github.com>
2021-11-01 00:18:39 +0300

    Version 0.1.7

--- Test_task_java_LevlUp/HospitalRegistration/src/main/java/com/example/hospitalregistration/entity/Recording.java
+++ Test_task_java_LevlUp/HospitalRegistration/src/main/java/com/example/hospitalregistration/entity/Recording.java
@@ -17,4 +17,28 @@ public class Recording {
         this.toVisit=toVisit;
         this.specialization=specialization;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Recording recording = (Recording) obj;
+        return(specialization == recording.getSpecialization() ||
+                (specialization != null && specialization.equals(recording.getSpecialization()))
+                && toVisit != null && toVisit.getTime() == recording.getToVisit().getTime()
+                );
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((specialization == null) ? 0 : specialization.hashCode());
+        result = prime * result + ((toVisit == null) ? 0 : toVisit.hashCode());
+        return result;
+    }
 }
