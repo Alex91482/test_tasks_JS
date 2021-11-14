f5a79a72dfa8ae07cdc2c7763a92d532b2f34509
Alex91482 <74872120+Alex91482@users.noreply.github.com>
2021-11-14 13:31:59 +0300

    Version 0.2.4

--- Test_task_java_LevlUp/HospitalRegistration/src/main/java/com/example/hospitalregistration/controller/DoctorsTimetableController.java
+++ Test_task_java_LevlUp/HospitalRegistration/src/main/java/com/example/hospitalregistration/controller/DoctorsTimetableController.java
@@ -2,19 +2,17 @@ package com.example.hospitalregistration.controller;
 
 import com.example.hospitalregistration.dao.DoctorsTimetableDAO;
 
+import com.example.hospitalregistration.service.patientservice.PatientForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -28,25 +26,29 @@ public class DoctorsTimetableController {
 
     @RequestMapping(value = "/doctorTimetable", method = RequestMethod.GET)
     public String showToMonthTimetable(Model model){
-
-        //строка format1.format(LocalDate.now().withDayOfMonth(1));
-        // означает что берем локальную дату переводим на первый день месяца и приводим к виду гггг.мм.дд
-
+            //строка format1.format(LocalDate.now().withDayOfMonth(1));
+            // означает что берем локальную дату переводим на первый день месяца и приводим к виду гггг.мм.дд
         String date = format1.format(LocalDate.now().withDayOfMonth(1));
         List<Map<String, Object>> list = doctorsTimetableDAO.getDoctorsTimetableFromMonth(date);
         model.addAttribute("doctorTimetable", list);
         return "pageTimetableDoctors"; //страница с расписанием врачей
     }
 
-    @RequestMapping(value = "/doctorNameTimetable/{lastNameDoctor}",method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> getRecordDoctorLastName(@PathVariable(name = "lastNameDoctor") String lastNameDoctor){
-        List<ArrayList<String>> list = doctorsTimetableDAO.getDoctorsTimetableFromLastName(lastNameDoctor);
-        return new ResponseEntity<>(list, HttpStatus.OK); //возвращаем расписание врачей по фамилии доктора
+    @RequestMapping(value = "/doctorNameTimetable",method = RequestMethod.POST) //метод отвечает за ввыод информации о расписании врача по фамилии
+    public String getRecordDoctorLastName(Model model, @RequestParam("lastNameDoctor") String lastNameDoctor){
+        List<Map<String, Object>> list = doctorsTimetableDAO.getDoctorsTimetableFromLastName(lastNameDoctor);
+        model.addAttribute("doctorTimetable", list);
+        return "pageTimetableDoctorsLast"; //возвращаем расписание врача по фамилии доктора
+    }
+    @RequestMapping(value="/doctorNameTimetable", method = RequestMethod.GET)
+    public String patientFrom(){
+        return "pageTimetableDoctorsLast"; //возвращаем страницу в которой нужно ввести фамилию доктора
     }
 
+    /*
     @RequestMapping(value = "/doctorNameTimetable/{id}",method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> getRecordDoctorId(@PathVariable(name = "id") long doctorId){
-        List<ArrayList<String>> list = doctorsTimetableDAO.getDoctorTimetableFromDoctorId(doctorId);
+        List<Map<String, Object>> list = doctorsTimetableDAO.getDoctorTimetableFromDoctorId(doctorId);
         return new ResponseEntity<>(list, HttpStatus.OK); //возвращаем расписание врачей по id доктора
     }
 
@@ -55,5 +57,6 @@ public class DoctorsTimetableController {
         List<Map<String, Object>> list = doctorsTimetableDAO.getDoctorsTimetableFromMonth(month);
         return new ResponseEntity<>(list, HttpStatus.OK); //возвращаем расписание врачей на месяц принимаем строку формата гггг.мм.дд что в принципе не обезательно т.к. происходит приведение к определенному формату
     }
+    */
 
 }
--- Test_task_java_LevlUp/HospitalRegistration/src/main/java/com/example/hospitalregistration/dao/DoctorsTimetableDAO.java
+++ Test_task_java_LevlUp/HospitalRegistration/src/main/java/com/example/hospitalregistration/dao/DoctorsTimetableDAO.java
@@ -1,7 +1,5 @@
 package com.example.hospitalregistration.dao;
 
-import com.example.hospitalregistration.mapper.DoctorsTimetableMapper;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,9 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -34,16 +30,14 @@ public class DoctorsTimetableDAO extends JdbcDaoSupport{
         //вернуть расписание дежурства врачей на месяц, месяц принемает формат гггг.мм.дд
         List<Map<String, Object>> list = new ArrayList<>();
         try{
-            if(date == null | date.equals("")){
+            if(date == null || date.equals("")){
                 System.out.println("Error date is null" + date);
                 return list;
             }
             Date date1 = formater.parse(date); //приводим к нужному формату
             String sql = "SELECT dt.Date, doctor.Last_name, doctor.First_name, doctor.specialization FROM doctors_timetable dt INNER JOIN doctor ON (dt.doctor_id = doctor.id)"
                     + " WHERE dt.Date >= ? ::date and dt.Date < ? ::date + '1 month'::interval";
-
-            Object[] param = {date1,date1};
-            list = getJdbcTemplate().queryForList(sql,param); //получаем Map где ключи это название колонок
+            list = getDoctorTimetableList(sql,date1);
 
         }catch (Exception e){
             logger.warn(e.getMessage());
@@ -51,57 +45,46 @@ public class DoctorsTimetableDAO extends JdbcDaoSupport{
         return list; //список хранит мапу где ключи String это название колонок из соедененных таблиц врачи и расписание_врачей
     }
 
-    public List<ArrayList<String>> getDoctorsTimetableFromLastName(String doctorLastName){
+    public List<Map<String, Object>> getDoctorsTimetableFromLastName(String doctorLastName){
         String sql = "SELECT * FROM doctors_timetable INNER JOIN doctor ON (doctors_timetable.doctor_id = doctor.id) WHERE doctor.last_name = ?";
-        List<ArrayList<String>> list = getDoctorTimetableList(sql,doctorLastName);
+        System.out.println(doctorLastName);
+        List<Map<String, Object>> list = getDoctorTimetableList(sql,doctorLastName);
+        System.out.println(list.size());
         return list; //создаем лист с вложенными листами где каждый вложенный лист хранит одну строку из сделанного запроса sql
     }
 
-    public  List<ArrayList<String>> getDoctorTimetableFromDoctorId(long id){
+    public  List<Map<String, Object>> getDoctorTimetableFromDoctorId(long id){
         //вернуть расписание по id доктора
         String sql = "SELECT * FROM doctors_timetable INNER JOIN doctor ON (doctors_timetable.doctor_id = doctor.id) WHERE doctor.id =?";
-        List<ArrayList<String>> list = getDoctorTimetableList(sql,id);
+        List<Map<String, Object>> list = getDoctorTimetableList(sql,id);
         return list; //аналогично как и с фафилией доктора
     }
 
-    private List<ArrayList<String>> getDoctorTimetableList(String sql, Object o){ //общий метод по запросу к бд с разницей по какому параметру будет запрос id или last name
-        List<ArrayList<String>> list = new ArrayList<>();
+    private List<Map<String, Object>> getDoctorTimetableList(String sql, Object o){ //общий метод по запросу к бд с разницей по какому параметру будет запрос id или last name
+        List<Map<String, Object>> list = new ArrayList<>(); //в мапе будут содержатся значения сторок (Object) с ключами которые обозначают названия колонок (String)
+
         try{
-            List<Map<String, Object>> strong = new ArrayList<>(); //в мапе будут содержатся значения сторок (Object) с ключами которые обозначают названия колонок (String)
             //проверяем что запрашивают
             if(o instanceof Long){ //если по id
                 long x = (Long) o;
-                strong = getJdbcTemplate().queryForList(sql, x);
+                list = getJdbcTemplate().queryForList(sql, x);
             } else if (o instanceof String){ //если по фамилии
                 String x = (String) o;
-                strong = getJdbcTemplate().queryForList(sql, x);
+                list = getJdbcTemplate().queryForList(sql, x);
             }
-            else if (o instanceof Date){
-                System.out.println("stage instance");
+            else if (o instanceof Date){ //если по месяцу
                 Date x = (Date) o;
-                //Date x =formater.format(one.getTime());
-                //Date x = (Date) o;
-                //String x =formater.format(one.getTime());
-                System.out.println("instance compelite " + x);
-                strong = getJdbcTemplate().queryForList(sql,new DoctorsTimetableMapper(),x,x); //getJdbcTemplate().query(sql, new DoctorsTimetableMapper(), date1, date1);
+                Object[] param = {x,x}; //в запросе sql содержится два параметра который на самом деле являестя одной и той же датой
+                list = getJdbcTemplate().queryForList(sql,param);
             }
             else{
                 logger.warn("Format error. Object format is not correct. Error in the block of doctors' schedule.");
                 return list; //если формат объекта не предусмотрен
             }
-            for (Map<String, Object> map : strong) { //получаем Map где ключи это название колонок
-                ArrayList<String> nestedLoop =new ArrayList<>();
-                //приводим object к Date потом форматируем по нужному образцу по умолчанию получаем string на выходе поэтому явное приведение в первой строке не нужно
-                nestedLoop.add(formater.format((Date)map.get("date")));
-                nestedLoop.add((String)map.get("last_name")); //по ключу (название колонки) получаем значение и явно приводим к формату String
-                nestedLoop.add((String)map.get("first_name"));
-                nestedLoop.add((String)map.get("specialization"));
-                list.add(nestedLoop); //список состоящий из строки выборки готов добавляем его в list
-            }
         }catch (Exception e){
             logger.warn(e.getMessage());
         }
-        return list; //возвращает список со списками который содержит строки, строки это одна строка из выборки
+        return list; //возвращает список с мапами в которых ключи это название колонок из сущностей по которым сделана выборка если не ясно то смотреть маперы для сущностей
     }
 
 
